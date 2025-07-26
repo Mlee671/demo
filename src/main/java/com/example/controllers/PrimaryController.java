49a1595ff7849df40a4f8a2f25f816dd0a67a23b
Mlee671 <mitchwill.lee@gmail.com>
2025-07-26 20:35:08 +1200

    removed unused imports from primary controller

--- src/main/java/com/example/controllers/PrimaryController.java
+++ src/main/java/com/example/controllers/PrimaryController.java
@@ -1,10 +1,8 @@
 package com.example.controllers;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.text.Font;
 
 // com.example.controllers.PrimaryController
 
